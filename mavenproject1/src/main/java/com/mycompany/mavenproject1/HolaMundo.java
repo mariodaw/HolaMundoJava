2088f8f520ae628e2a1f66c2fd82ff9a2e24f9f2
mariodaw <mariotomas@gmail.com>
2021-11-22 18:49:10 +0100

    He escrito un sout para que diga "Hola Mundo", que es el proposito del programa

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/HolaMundo.java
@@ -0,0 +1,24 @@
+/*
+ * Proyecto mavenproject1 - Archivo HolaMundo.java - Compañia DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+*
+*@author Mario Tomas Zanon <newmariotomas@gmail.com>
+*@version 1.0
+*@date 22 nov. 2021 18:45:27
+*/
+/**
+ *
+ * @author Mario Tomas Zanon <newmariotomas@gmail.com>
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+
+}
